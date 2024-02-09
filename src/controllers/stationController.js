a24de48228ab8b103e103c9119c0cd7ff594b9fc
jubaer <jubaerhosain1119@gmail.com>
2024-02-09 17:35:46 +0600

    add station done

--- src/controllers/stationController.js
+++ src/controllers/stationController.js
@@ -1,5 +1,27 @@
+import { Joi } from "../utils/Joi.js";
+import { models } from "../configs/mysql.js";
 
 async function addStation(req, res) {
+    const stationDto = req.body;
+    const stationSchema = 
+        Joi.object({
+            station_id: Joi.number().integer().required(),
+            station_name: Joi.string().required(),
+            longitude: Joi.number().required(),
+            latitude: Joi.number().required()
+        }).required();
+
+    const { error } = stationSchema.validate(stationDto);
+    if (error) return res.status(400).json(error.details);
+
+    if (stationDto.station_id) {
+        const existed = await models.Station.findByPk(stationDto.station_id);
+        if (existed) return res.status(409).json({ message: `station with id: ${stationDto.station_id} already exists` });
+    }
+
+    const station = await models.Station.create(stationDto);
+
+    res.status(201).json(station);
 }
 
 async function getAllStations(req, res) {
