433c51d6484c0130dd87a926ff37db23702d2c8f
JubaerHosain <jubaerhosain1119@gmail.com>
2024-02-11 01:21:01 +0600

    updated redis config

--- src/configs/redis.js
+++ src/configs/redis.js
@@ -7,14 +7,28 @@ export function initializeRedisConnection() {
     redisClient.on("error", (err) => {
         console.error("Redis Client Error:", err.message);
     });
-    
+
     redisClient.on("connect", () => {
         console.log("Redis connection has been established successfully.");
     });
-    
+
     redisClient.on("close", () => {
         console.log("Redis connection closed.");
     });
+
+    // key name convention ${entity:id}
+    setTimeout(() => {
+        redisClient.set("user:1", JSON.stringify({ id: 1, name: "jubaer", email: "jubaer@gmail.com" }), (err, result) => {
+            if(err) console.log(err.message);
+        });
+    }, 2000);
+
+    setTimeout(async () => {
+        const res = await redisClient.get("user:1");
+        console.log(JSON.parse(res));
+    }, 3000);
+
+
 }
 
 export default redisClient;
