4990c233df9709e1450b1895c773285de2083d1b
JubaerHosain <jubaerhosain1119@gmail.com>
2024-02-12 14:47:46 +0600

    updated

--- email-server/src/config/amqp.js
+++ email-server/src/config/amqp.js
@@ -2,6 +2,7 @@ import amqp from "amqplib";
 import config from "./config.js";
 
 export async function getAmqpConnectionAndChannel() {
+    // implement reconnection strategies using connection.on("error");
     const connection = await amqp.connect(`amqp://${config.rabbitmq.host}`);
     const channel = await connection.createChannel();
     return { connection, channel };
