7f19ca5ec7999fae028b20b025c0995f3266cd99
JubaerHosain <jubaerhosain1119@gmail.com>
2024-02-13 18:59:04 +0600

    updated

--- api-server/src/middlewares/fileUploader.js
+++ api-server/src/middlewares/fileUploader.js
@@ -5,6 +5,6 @@ const storage = multer.diskStorage({});
 export const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 1024 * 1024 * 5 // Limit file size to 5MB
-    }
+        fileSize: 1024 * 1024 * 5, // Limit file size to 5MB
+    },
 });
